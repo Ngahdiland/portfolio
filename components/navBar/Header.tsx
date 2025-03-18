f3db3f744e942c31c3d90ec2111d756303eb0455
ngahdiland <fonyuydiland@gmail.com>
2025-03-18 16:25:54 +0100

    small updates

--- components/navBar/Header.tsx
+++ components/navBar/Header.tsx
@@ -2,27 +2,28 @@ import Link from "@/node_modules/next/link";
 import React from "react";
 import Image from "@/node_modules/next/image";
 import Profile from "../../public/assets/pro.png";
+import "./style.css"
 
 const Header = () => {
   return (
     <div className="flex flex-col sm:flex-row mt-[40px]">
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#000] flex flex-col w-[90%] ml-[5%]">
-          <div className="flex items-center gap-2 ">
+          <div className="toUP flex items-center gap-2 ">
             <p className="w-8 md:w-11 h-[2px] bg-[#000]"></p>
-            <p className="font-medium text-sm md:text-base">Hi There!</p>
+            <p className=" font-medium text-[40px] ">Hi There!</p>
           </div>
-          <h1 className="prata-regular text-6xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-[70px] sm:py-3 leading-relaxed font-bold">
             I'M <span className="text-amber-500 ">NGAH DILAND</span>
           </h1>
-          <p className="prata-regular text-2xl sm:text-sm flex justify-center mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora eaque qui, doloremque sint nemo sequireiciendis assumenda minima? Quam, corrupti?</p>
+          <p className="divOne prata-regular text-[30px] flex justify-center mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora eaque qui, doloremque sint nemo sequireiciendis assumenda minima? Quam, corrupti?</p>
           <div className="flex items-center gap-2">
             <Link href="/about">
-              <p className="font-semibold text-sm md:text-base">
+              <p className="font-semibold text-[30px] underline italic">
                 MORE ABOUT ME.
               </p>
             </Link>
-            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            <p className="w-8 md:w-11 h-[2px] bg-[#000000]"></p>
           </div>
         </div>
       </div>
