68fb83310cabaa5cc65dea25b21a5b90908b9db3
ngahdiland <fonyuydiland@gmail.com>
2025-03-18 15:23:35 +0100

    complete resume responce

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -8,22 +8,22 @@ const Resume = () => {
         <h1 className='text-9xl font-bold'>RESUME</h1>
       </div>
       <div className="">
-        <div className="flex gap-7">
-          <div className="w-1/3">
+        <div className=" main flex gap-7">
+          <div className="mainDiv w-1/3">
             <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
             <h2 className='text-3xl text-amber-50'>BootCamp</h2>
             </div>
             <h3 className='text-3xl font-bold pb-4'>what is BootCamp</h3>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
           </div>
-          <div className="w-1/3">
+          <div className="mainDiv w-1/3">
             <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
             <h2 className='text-3xl text-amber-50'>BootCamp</h2>
             </div>
             <h3 className='text-3xl font-bold pb-4'>what is BootCamp</h3>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
           </div>
-          <div className="w-1/3">
+          <div className="mainDiv w-1/3">
             <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
             <h2 className='text-3xl text-amber-50'>BootCamp</h2>
             </div>
@@ -31,22 +31,22 @@ const Resume = () => {
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
           </div>
         </div>
-        <div className="flex gap-7 mt-[30px] mb-[80px]">
-          <div className="w-1/3">
+        <div className="main flex gap-7 mt-[30px] mb-[80px]">
+          <div className="mainDiv w-1/3">
+            <div className="col w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
+            <h2 className='text-3xl text-amber-50'>BootCamp</h2>
+            </div>
+            <h3 className='text-3xl font-bold pb-4'>what is BootCamp</h3>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
+          </div>
+          <div className="mainDiv w-1/3">
             <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
             <h2 className='text-3xl text-amber-50'>BootCamp</h2>
             </div>
             <h3 className='text-3xl font-bold pb-4'>what is BootCamp</h3>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
           </div>
-          <div className="w-1/3">
-            <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
-            <h2 className='text-3xl text-amber-50'>BootCamp</h2>
-            </div>
-            <h3 className='text-3xl font-bold pb-4'>what is BootCamp</h3>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis aliquam, fugit necessitatibus odio voluptate a cum ad sed, quae porro ducimus nobis ratione neque dolorem aperiam rem quibusdam perferendis.</p>
-          </div>
-          <div className="w-1/3">
+          <div className=" mainDiv w-1/3">
             <div className="w-[200px] bg-amber-500 h-[40px] flex flex-col justify-center items-center shadow shadow-gray-950 rounded-xl mb-4">
             <h2 className='text-3xl text-amber-50'>BootCamp</h2>
             </div>
