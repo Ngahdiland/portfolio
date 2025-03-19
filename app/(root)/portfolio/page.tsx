54d828c211993f89ff4e37b4a83df667114ec1c9
ngahdiland <fonyuydiland@gmail.com>
2025-03-19 01:45:57 +0100

    done with UI

--- app/(root)/portfolio/page.tsx
+++ app/(root)/portfolio/page.tsx
@@ -1,15 +1,125 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
+import one from "../../../public/assets/one-inter.png";
+import two from "../../../public/assets/web-assignment.png";
+import three from "../../../public/assets/grid.png";
+import four from "../../../public/assets/web.png";
+import five from "../../../public/assets/forever.png";
+import six from "../../../public/assets/ig.png";
+import seven from "../../../public/assets/beer.png";
+import eight from "../../../public/assets/data.png";
+import nine from "../../../public/assets/data-2.png";
+import ten from "../../../public/assets/data-3.png";
+import eleven from "../../../public/assets/data-4.png";
+import twelve from "../../../public/assets/facebook.png";
+import Image from "@/node_modules/next/image";
 
 const Portfolio = () => {
   return (
-    <div className='flex flex-col gap-9 w-3/4 mt-[80px] mx-[50px]'>
+    <div className="flex flex-col gap-9 w-3/4 mt-[80px] mx-[50px]">
       <div className=" about flex h-[160px] border-2 border-gray-900  justify-center items-center ">
-        <h1 className='text-9xl font-bold'>PORTFOLIO</h1>
+        <h1 className="text-9xl font-bold">PORTFOLIO</h1>
       </div>
-      
+      <div className=" flex flex-col gap-4">
+        <h2 className="  text-2xl font-bold">
+          MY <span className="text-amber-500 ">PROJECTS</span>
+        </h2>
+        <div className="flex gap-4 tosend">
+          <div className="w-1/4 tothere">
+            <a
+              href="https://github.com/Ngahdiland/one-next-inter.git"
+              className=""
+            >
+              <Image src={one} alt="" />
+            </a>
+          </div>
+          <div className="w-1/4 tothere">
+            <a
+              href="https://github.com/Ngahdiland/second-year-ass.git"
+              className=""
+            >
+              <Image src={two} alt="" />
+            </a>
+          </div>
+          <div className="w-1/4 tothere">
+            <a href="https://github.com/Ngahdiland/grid-temp.git" className="">
+              <Image src={three} alt="" />
+            </a>
+          </div>
+          <div className="w-1/4 tothere">
+            <a
+              href="https://github.com/Ngahdiland/school-work.git"
+              className=""
+            >
+              <Image src={four} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4 tosend">
+        <div className="w-1/4 tothere">
+          <a href="https://github.com/Ngahdiland/forever-ecom.git" className="">
+            <Image src={five} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/instagram-profile.git"
+            className=""
+          >
+            <Image src={six} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a href="https://github.com/Ngahdiland/gui-tkinter.git" className="">
+            <Image src={seven} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/data-analysis.git"
+            className=""
+          >
+            <Image src={eight} alt="" />
+          </a>
+        </div>
+      </div>
+      <div className="flex gap-4 mb-[30px] tosend">
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/data-analysis.git"
+            className=""
+          >
+            <Image src={nine} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/data-analysis.git"
+            className=""
+          >
+            <Image src={ten} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/data-analysis.git"
+            className=""
+          >
+            <Image src={eleven} alt="" />
+          </a>
+        </div>
+        <div className="w-1/4 tothere">
+          <a
+            href="https://github.com/Ngahdiland/facebook-profile.git"
+            className=""
+          >
+            <Image src={twelve} alt="" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- components/navBar/Header.tsx
+++ components/navBar/Header.tsx
@@ -8,19 +8,19 @@ const Header = () => {
   return (
     <div className="flex flex-col sm:flex-row mt-[40px]">
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
-        <div className="text-[#000] flex flex-col w-[90%] ml-[5%]">
+        <div className="text-[#000] flex flex-col w-[80%] ml-[10%]">
           <div className="toUP flex items-center gap-2 ">
             <p className="w-8 md:w-11 h-[2px] bg-[#000]"></p>
             <p className=" font-medium text-[40px] ">Hi There!</p>
           </div>
-          <h1 className="prata-regular text-[70px] sm:py-3 leading-relaxed font-bold">
+          <h1 className="hOne prata-regular text-[70px] sm:py-3 leading-relaxed font-bold">
             I'M <span className="text-amber-500 ">NGAH DILAND</span>
           </h1>
           <p className="divOne prata-regular text-[30px] flex justify-center mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora eaque qui, doloremque sint nemo sequireiciendis assumenda minima? Quam, corrupti?</p>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 ">
             <Link href="/about">
-              <p className="font-semibold text-[30px] underline italic">
-                MORE ABOUT ME.
+              <p className="toEdit font-semibold w-[300px] h-[40px] bg-amber-500 text-[30px] flex flex-col justify-center items-center rounded-xl underline italic">
+                MORE ABOUT ME
               </p>
             </Link>
             <p className="w-8 md:w-11 h-[2px] bg-[#000000]"></p>
