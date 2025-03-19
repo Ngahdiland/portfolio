bb8be944d9d63c273ad2b16b3bdbbc5042c4c929
ngahdiland <fonyuydiland@gmail.com>
2025-03-19 10:45:01 +0100

    done with UI link

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import facebook from "../../../public/assets/folder-open-outline.svg";
 
 const Contact = () => {
   return (
@@ -22,38 +23,58 @@ const Contact = () => {
         <div className="black-div w-full h-[320px] text-amber-50 pt-5 bg-gray-900 mb-[50px]">
           <div className=" block-one flex justify-center items-center w-[90%] mx-auto my-auto">
             <div className="col-1 w-1/3 h-[140px] flex justify-center items-center flex-col border-b-2 border-amber-50">
-              <h1 className="text-6xl">3 +</h1>
-              <h3 className="text-3xl">Years Expirience</h3>
+              <h1 className="text-5xl">GitHub</h1>
+              <h3 className="text-3xl">
+                <a href="https://github.com/Ngahdiland">@ngahdiland</a>
+              </h3>
             </div>
             <div className="col-1 w-1/3 h-[140px] flex justify-center items-center flex-col border-l-2 border-b-2 border-amber-50">
-              <h1 className="text-6xl">30 +</h1>
-              <h3 className="text-3xl">Projects Done</h3>
+              <h1 className="text-5xl">FACEBOOK</h1>
+              <h3 className="text-3xl">
+                <a href="https://www.facebook.com/profile.php?id=100085739285135">
+                  @ngahdiland
+                </a>
+              </h3>
             </div>
             <div className="col-1 w-1/3 h-[140px] flex justify-center items-center flex-col border-l-2 border-b-2 border-amber-50">
-              <h1 className="text-6xl">30 +</h1>
-              <h3 className="text-3xl">Projects Done</h3>
+              <h1 className="text-5xl">INSTAGRAM</h1>
+              <h3 className="text-3xl">
+                <a href="https://www.facebook.com/profile.php?id=100085739285135">
+                  @ngahdiland
+                </a>
+              </h3>
             </div>
           </div>
           <div className=" block-one flex justify-center items-center w-[90%] mx-auto my-auto">
             <div className="col-1 w-1/3 h-[140px] flex justify-center items-center flex-col border-amber-50">
-              <h1 className="text-6xl">10 +</h1>
-              <h3 className="text-3xl">Happy Clients</h3>
+              <h1 className="text-5xl">X</h1>
+              <h3 className="text-3xl">
+                <a href="https://x.com/Ngahdiland?t=Kww3Xq01zRVSWnEMU9u11w&s=09">
+                  @ngahdiland
+                </a>
+              </h3>
             </div>
             <div className="col-1  w-1/3 h-[140px] flex justify-center items-center flex-col border-l-2 border-amber-50">
-              <h1 className="text-6xl">1.2K +</h1>
-              <h3 className="text-3xl">Followers</h3>
+              <h1 className="text-5xl">PHONE</h1>
+              <h3 className="text-3xl">
+                +237 677828170 <br />
+                +237 654794823
+              </h3>
             </div>
             <div className="col-1 noBorder w-1/3 h-[140px] flex justify-center items-center flex-col border-l-2 border-amber-50">
-              <h1 className="text-6xl">30 +</h1>
-              <h3 className="text-3xl">Projects Done</h3>
+              <h1 className="text-5xl">WHATSAPP</h1>
+              <h3 className="text-3xl">
+                <a href="https://wa.me/+237654794823">
+                  +237 677828170 <br />
+                  +237 654794823
+                </a>
+              </h3>
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center ">
-        <h2 className="  text-4xl font-bold pb-9">
-          THANKS FOR THE PATIENCE!
-        </h2>
+        <h2 className="  text-4xl font-bold pb-9">THANKS FOR THE PATIENCE!</h2>
       </div>
     </div>
   );
